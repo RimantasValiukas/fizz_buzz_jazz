171a3231287e404ef2e6942884693c335657324d
RimantasValiukas <rimantas.valiukas@gmail.com>
2024-02-19 14:39:21 +0200

    first commit

---
+++ src/main/java/FizzBuzzJazz.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class FizzBuzz {
+
+    private final int n;
+    private final Map<Integer, String> divisors;
+
+    public FizzBuzz(int n) {
+        this.n = n;
+        divisors= new TreeMap<>(Map.of(3, "Fizz", 5, "Buzz", 7, "Jazz"));
+        printResult();
+    }
+
+    private List<String> fizzBuzz() {
+        List<String> result = new ArrayList<>();
+
+        for (int i = 1; i <= n; ++i) {
+            StringBuilder print = new StringBuilder();
+
+            for (Integer key : divisors.keySet()) {
+                if (i % key == 0) {
+                    print.append(divisors.get(key));
+                }
+            }
+
+            if (print.isEmpty()) {
+                print.append(i);
+            }
+
+            result.add(print.toString());
+        }
+
+        return result;
+    }
+
+    private void printResult() {
+        List<String> results = fizzBuzz();
+
+        for (String s : results) {
+            System.out.print(s + " ");
+        }
+    }
+
+}
